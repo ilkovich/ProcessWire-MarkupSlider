689ea4464f2490c1f79c4403d47375dd9db5444a
Daniel Ilkovich <dilkovich@thoughtvineinc.com>
2012-09-10 11:31:33 -0400

    async loading of images

--- MarkupSlider.js
+++ MarkupSlider.js
@@ -1,5 +1,28 @@
 $(window).load(function() {
+    /*
     $('.nivoSlider').nivoSlider({
-        effect: 'fade'
+    effect: 'fade'
     });
+    */
+
+    function runNivoSlider(id) {
+        $('#'+id).parent().parent().children("img").prependTo('#'+id);
+        $('#'+id).nivoSlider({
+            effect: 'fade'
+        });
+    }
+
+    for(i in NivoSlider.sliders) {
+        var $slider= $('#'+i);
+        NivoSlider.sliders[i].loaded=1;
+        for(j=0; j<NivoSlider.sliders[i].length; j++) {
+            var img = NivoSlider.sliders[i][j];
+            $img = $slider.append(img).children().last();
+            $img.load(function() {
+                NivoSlider.sliders[i].loaded++; 
+                //if(NivoSlider.sliders[i].loaded === NivoSlider.sliders[i].length) 
+                runNivoSlider(i);
+            });
+        }
+    }
 });
